16bad7b8a30dc6863eab9f03b99d2f5adedf97c8
nCabanillas <n.cblls@gmail.com>
2023-06-20 13:35:57 -0500

    Prob 1,2,3,4

---
+++ main.py
@@ -0,0 +1,24 @@
+from solver_module.solver_problems import solve_problems
+
+def switcher_function(case:int):
+    switch_dic = {
+        1: solve_problems.problem_1,
+        2: solve_problems.problem_2,
+        3: solve_problems.problem_3,
+        4: solve_problems.problem_4,
+        5: solve_problems.problem_5,
+        6: solve_problems.problem_6,
+        7: solve_problems.problem_7,
+        8: solve_problems.problem_8,
+    }
+    return switch_dic.get(case, lambda: ' no hay esa opción')()
+    
+if __name__ == '__main__':
+    try:
+        case = int(input('Ingrese el número correspondiente al problema que desea resolver:'))
+    except ValueError:
+        print(f'\nEl valor ingresado debe ser un número entero\n')
+    except Exception as e:
+        print(f'\nOcurrió el siguiente error ==> {e}\n')
+    else:
+        switcher_function(case)    
